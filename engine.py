8357e9fb19f5fa81fb14ef74f847aef8e7f16551
norman081012-create <norman081012@gmail.com>
2026-04-16 19:33:44 +0800

    Update engine.py

--- engine.py
+++ engine.py
@@ -45,7 +45,6 @@ class GameEngine:
         self.p1_proposals = {'R': None, 'H': None}
         self.p1_selected_plan = None
         
-        # 📌 修正：初始設定，Ruling 永遠是 R-Role
         self.ruling_party = self.party_A
         self.r_role_party = self.ruling_party
         self.h_role_party = self.party_B if self.ruling_party.name == self.party_A.name else self.party_A
@@ -56,6 +55,7 @@ class GameEngine:
         self.proposal_count = 1
         self.proposing_party = self.party_A
         self.history = []
+        self.swap_triggered_this_year = False
         self.last_year_report = None
         
         self.boundary_B = 100 
@@ -66,7 +66,7 @@ class GameEngine:
             'Year': self.year, 'GDP': self.gdp, 'Sanity': self.sanity, 'Emotion': self.emotion,
             'A_Support': self.party_A.support, 'B_Support': self.party_B.support,
             'A_Wealth': self.party_A.wealth, 'B_Wealth': self.party_B.wealth,
-            'Is_Election': is_election, 'Is_Swap': False, # 移除 Swap 紀錄
+            'Is_Election': is_election, 'Is_Swap': self.swap_triggered_this_year,
             'Ruling': self.ruling_party.name, 'H_Party': self.h_role_party.name,
             'R_Party': self.r_role_party.name,
             'A_Edu': float(self.party_A.edu_stance),
@@ -74,6 +74,7 @@ class GameEngine:
             'A_Avg_Abi': (self.party_A.build_ability + self.party_A.investigate_ability + self.party_A.media_ability + self.party_A.predict_ability + self.party_A.stealth_ability + self.party_A.edu_ability)/6,
             'B_Avg_Abi': (self.party_B.build_ability + self.party_B.investigate_ability + self.party_B.media_ability + self.party_B.predict_ability + self.party_B.stealth_ability + self.party_B.edu_ability)/6
         })
+        self.swap_triggered_this_year = False
 
 def execute_poll(game, view_party, cost):
     view_party.wealth -= cost
@@ -88,3 +89,13 @@ def execute_poll(game, view_party, cost):
     game.party_B.latest_poll = b_poll
     game.party_B.poll_history[poll_type].append(b_poll)
     view_party.poll_count += 1
+
+# 📌 恢復 Swap 換位機制
+def trigger_swap(game, penalty_amt, msg_prefix="Political Turmoil!"):
+    game.party_A.wealth -= penalty_amt; game.party_B.wealth -= penalty_amt
+    game.h_role_party, game.r_role_party = game.r_role_party, game.h_role_party
+    game.swap_triggered_this_year = True
+    
+    st.session_state.news_flash = f"🗞️ **[BREAKING] {msg_prefix}** Both parties forced to pay {penalty_amt:.1f} to charities, triggering an immediate Cabinet Swap!"
+    st.session_state.anim = 'snow'
+    game.phase = 2
